5621c38b48c09f251c8f8951ef1db72c174e3335
xinzhi_lff <1076747468@qq.com>
2018-07-06 15:19:37 +0800

    ''

---
+++ myVueCliServer/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header style="height: 44px;background-color: cornflowerblue;"></header>
+    <main style="height: 500px;">
+      <router-view></router-view>
+    </main>
+    <footer style="height: 44px;background-color: #000;"></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ myVueCliServer/src/views/home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <h1>首页11</h1>
+    <h1>首页11</h1>
+    <h1>首页11</h1>
+    <h1>首页11</h1>
+    <p v-text="message"></p>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      message: new Date().getTime()
+    }
+  },
+  pageConfig () {
+    return {
+      title: 'home',
+      bodyStyle: 'background-color:  #fff;'
+    }
+  },
+  created () {
+  },
+  mounted () {
+
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style type="text/css">
+
+</style>
