7ba26e4fd394b3f99db54c5af72bb29f97049b11
xinzhi_lff <1076747468@qq.com>
2018-07-06 18:02:03 +0800

    '服务端渲染'

--- myVueCliServer/src/views/home.vue
+++ myVueCliServer/src/views/home.vue
@@ -9,6 +9,7 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   data: () => {
     return {
@@ -24,7 +25,26 @@ export default {
   created () {
   },
   mounted () {
-
+    console.log(axios)
+    const instance = axios.create({
+      /* baseURL: 'http://www.server.com', */
+      timeout: 50000,
+      // headers: {'Content-Type': 'application/json;charset=UTF-8'}
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    })
+    instance.interceptors.response.use((res) => {
+      if (res.status >= 200 && res.status < 300) {
+        return res
+      }
+      return Promise.reject(res)
+    }, (error) => {
+      return Promise.reject({message: error.message || '网络异常，请刷新重试', code: 404})
+    })
+    instance.get('api/index.php').then(res => {
+      console.log(res)
+    }).catch((error) => {
+      console.log(error)
+    })
   },
   methods: {},
   components: {}
